e735c4ba59ced6724168fd4f3c4b0a704065d7a6
Tiescoolio <ties.smid@student.hu.nl>
2024-11-06 09:47:58 +0100

    commentaar bij de code gezet

--- Dashboard/dashboard_Gui.py
+++ Dashboard/dashboard_Gui.py
@@ -7,47 +7,55 @@ import pandas as pd
 
 def gui():
     global time_label
-    global weather_information
+    global weather_label
     global disruptions
 
+    # Gegevens van de window opgeven
     dashboard_window = tk.Tk()
     dashboard_window.title("Dashboard")
     dashboard_window.geometry("800x600")
     dashboard_window.configure(background="#f7d417")
     dashboard_window.attributes('-fullscreen', True)
 
+    # Icoon aanmaken voor de window
     icon = tk.PhotoImage(file="proRail_Logo.png")
     dashboard_window.iconphoto(False, icon)
 
+    # Label aanmaken voor de storingen
     disruptions = tk.Label(dashboard_window, text="", background="#f7d417", foreground="#003373",
                            font=("Helvetica", 16, "bold italic"))
     disruptions.pack(pady=60)
 
+    # Label aanmaken voor de tijd
     time_label = tk.Label(dashboard_window, background="#f7d417", foreground="#003373",
                           font=("Helvetica", 20, "bold italic"))
     time_label.pack(pady=20)
 
-    weather_information = tk.Label(dashboard_window, text="", background="#f7d417", foreground="#003373",
-                                   font=("Helvetica", 16, "bold italic"))
-    weather_information.pack(pady=20)
+    # Label aanmaken voor het weer
+    weather_label = tk.Label(dashboard_window, text="", background="#f7d417", foreground="#003373",
+                             font=("Helvetica", 16, "bold italic"))
+    weather_label.pack(pady=20)
 
+    # Knop aanmaken om een storing in te voeren
     new_disruption = tk.Button(dashboard_window, text="Storing Invoeren", command=ns)
     new_disruption.pack(pady=20)
 
+    # Update de tijd, weer en storing labels herhalend
     update_time()
     update_weatherinformation()
     update_disruptions()
 
     dashboard_window.mainloop()
 
-
 def update_time():
+    # Update de tijd label om de 0.2 seconden met de nieuwe tijd
     time = datetime.datetime.now().strftime("%H:%M:%S")
     time_label.config(text=time)
     time_label.after(200, update_time)
 
 
 def update_weatherinformation():
+    # Update het weer label om de minuut met het weer, met de HU als locatie
     latitude = 52.0990
     longitude = 5.1091
     api = "811567671a6186280e165c81ca4f0c93"
@@ -60,14 +68,15 @@ def update_weatherinformation():
     max_temperature = data["main"]["temp_max"]
     weather = data['weather'][0]['description']
 
-    weather_information.config(text=f"Temperatuur: {temperature} Graden Celsius\n"
+    weather_label.config(text=f"Temperatuur: {temperature} Graden Celsius\n"
                                     f"Minimale Temperatuur: {min_temperature} Graden Celsius\n"
                                     f"Maximale Temperatuur: {max_temperature} Graden Celsius\n"
                                     f"Weer: {weather}")
-    weather_information.after(60000, update_weatherinformation)
+    weather_label.after(60000, update_weatherinformation)
 
 
 def update_disruptions():
+    # Update de storingen label om de 5 seconden met de laatste 3 storingen uit het bestand
     df = pd.read_csv("storing_Gegevens_3_features.csv", sep=";")
     display_disruptions = df.tail(3)
     disruption_display_text = ""
--- Dashboard/inlog_Systeem.py
+++ Dashboard/inlog_Systeem.py
@@ -6,38 +6,46 @@ def gui():
     global password_entry
     global login_window
 
+    # Gegevens van de window opgeven
     login_window = tk.Tk()
     login_window.title("Inlog Systeem")
     login_window.configure(background="#f7d417")
     login_window.geometry("300x250+600+250")
 
+    # Icoon aanmaken voor de window
     icon = tk.PhotoImage(file="proRail_Logo.png")
     login_window.iconphoto(False, icon)
 
+    # Override de toolbar bovenin een window
     login_window.overrideredirect(True)
 
+    # Label en entry aanmaken waar de gebruikersnaam ingevoerd kan worden
     username_label = tk.Label(login_window, text="Gebruikersnaam", background="#f7d417", foreground="#003373",
                               font=("Helvetica", 16, "bold italic"))
     username_label.pack(pady=5)
     username_entry = tk.Entry(login_window)
     username_entry.pack(pady=5)
 
+    # Label en entry aanmaken waar het wachtwoord ingevoerd kan worden
     password_label = tk.Label(login_window, text="Wachtwoord", background="#f7d417", foreground="#003373",
                               font=("Helvetica", 16, "bold italic"))
     password_label.pack(pady=5)
     password_entry = tk.Entry(login_window, show="*")
     password_entry.pack(pady=5)
 
+    # Knop aanmaken voor het inloggen, en deze command ook verbinden met de "enter" toets
     login_button = tk.Button(login_window, text="Inloggen", command=login)
     login_button.pack(pady=10)
     login_window.bind('<Return>', lambda event: login())
 
+    # Knop aanmaken voor het aanmaken van een nieuwe gebruiker
     new_user_button = tk.Button(login_window, text="Nieuwe gebruiker", command=new_user)
     new_user_button.pack(pady=10)
 
     login_window.mainloop()
 
 def new_user():
+    # Pak de inputs van de entries en zet ze in variabelen
     username = username_entry.get()
     password = password_entry.get()
 
@@ -45,7 +53,7 @@ def new_user():
         messagebox.showwarning("Fout", "Vul zowel gebruikersnaam als wachtwoord in!")
         return
 
-
+    # Zorg dat er niet meerdere gebruikers met dezelfde gebruikersnaam kunnen zijn
     with open("inlog_Gegevens.txt", "r") as file:
         lines = file.readlines()
         for line in lines:
@@ -54,6 +62,7 @@ def new_user():
                 messagebox.showwarning("Fout", "Gebruiker bestaat al!")
                 return
 
+    # Slaat de nieuwe gebruiker op in de text file
     with open("inlog_Gegevens.txt", "a") as file:
         file.write(f"{username},{password}\n")
     messagebox.showinfo("Succes", "Gebruiker aangemaakt!")
@@ -61,9 +70,11 @@ def new_user():
     password_entry.delete(0, tk.END)
 
 def login():
+    # Pak de inputs van de entries en zet ze in variabelen
     username = username_entry.get()
     password = password_entry.get()
 
+    # Kijkt of het wachtwoord past bij de gebruikersnaam, zo ja word de persoon ingelogd
     with open("inlog_Gegevens.txt", "r") as file:
         lines = file.readlines()
         for line in lines:
